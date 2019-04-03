8ad509b430ce028e8a7b0d048ad8602b04d95f01
nbeers22 <nbeers22@gmail.com>
2019-04-03 13:11:13 +0200

    Initial commit of basic layout wireframe

---
+++ app.js
@@ -0,0 +1,65 @@
+const STORE = [
+  {
+    id: 1,
+    question: "Who was the two-time Heisman trophy winner at Ohio State?",
+    answer: "Archie Griffin",
+    choices: ["Eddie George", "Archie Griffin","Orlando Pace","Tippy Dye"]
+  },
+  {
+    id: 2,
+    question: "When was the last time Ohio State won the national championship?",
+    answer: 2014,
+    choices: [1998,2002,2010,2014]
+  },
+  {
+    id: 3,
+    question: "Who was the Buckeyes' coach in 2007?",
+    answer: "Jim Tressel",
+    choices: ["Earl Bruce", "John Cooper","Urban Meyer","Jim Tressel"]
+  },
+  {
+    id: 4,
+    question: "What was John Cooper's record against Michigan?",
+    answer: "1-11-1",
+    choices: ["1-12-1", "1-11-1","2-10-1","2-9-1"]
+  },
+  {
+    id: 5,
+    question: "Who was the starting quarterback for Ohio State during the 2002 season?",
+    answer: "Craig Krenzel",
+    choices: ["Joe Germaine", "Steve Bellisari","Craig Krenzel","Todd Boeckman"]
+  },
+  {
+    id: 6,
+    question: "In 1998, Ohio State lost only 1 game. Who did they lose to?",
+    answer: "Michigan State",
+    choices: ["Iowa", "Wisconsin","Michigan","Michgan State"]
+  },
+  {
+    id: 7,
+    question: "Which school did John Cooper coach prior to Ohio State?",
+    answer: "Arizona State",
+    choices: ["Arizona State", "California","New Mexico","UNLV"]
+  },
+  {
+    id: 8,
+    question: "Which year did Woody Hayes retire",
+    answer: 1978,
+    choices: [1976,1977,1978,1979]
+  },
+  {
+    id: 9,
+    question: "Which Buckeye made the 'pancake' famous?",
+    answer: "Orlando Pace",
+    choices: ["LeCharles Bentley", "Orlando Pace","Mike Doss","Bill Cower"]
+  },
+  {
+    id: 10,
+    question: "In the 1990s, the Buckeyes had several All-Americans. Which of the following Buckeye linebackers was never an All-American in the decade?",
+    answer: "Greg Bellisari",
+    choices: ["Andy Katzenmoyer", "Na'il Diggs","Steve Tovar","Greg Bellisari"]
+  }
+]
+
+
+
